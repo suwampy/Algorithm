c2a8cd7529d61ef34327d4577fa27cb91895a8ca
suwanyu <tndhks123@nate.com>
2020-10-09 11:41:05 +0900

    프로그래머스 level 1 수박수박수, 두 정수 사이 합

---
+++ src/programmers/level1/subak.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+public class subak {
+    public static void main(String[] args) {
+        int n = 4;
+        //길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+        // 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+
+        String answer ="";
+        String soo = "수";
+        String bak = "박";
+
+        for (int i=1; i<=n; i++){
+            if(i%2!=0){
+                answer+=soo;
+            }else{
+                answer += bak;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    //다른 풀이이
+}
