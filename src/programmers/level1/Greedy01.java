85a014d4eb093ce8ba3616782787015f1c714256
suwanyu <tndhks123@nate.com>
2020-10-15 22:43:09 +0900

    하노이의 탑

---
+++ src/programmers/level1/Greedy01.java
@@ -0,0 +1,20 @@
+package programmers.level1;
+
+public class Greedy01 {
+    public static void main(String[] args) {
+        // n = 전체 학생의 수
+        // lost[] = 체육복을 도난당한 학생들의 번호
+        // reserve[] = 여벌의 체육복을 가져온 학생들의 번호가 담긴 번호
+        // solution() 체육수업을 들을수 있는 학생의 최댓값 return
+
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {3};
+        int num = 5;
+
+
+
+        System.out.println("n =" + num);
+
+    }
+}
